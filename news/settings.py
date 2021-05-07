59391f66e5bd1d02d579458c2d2eab28aa7e2cfc
host1k <bobah271999@gmail.com>
2021-05-07 10:18:39 +0300

    Deploy

--- news/settings.py
+++ news/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,7 @@ SECRET_KEY = '+98qov1v%fx7shl)m&e(sn_0zyonl+f%uxmh^0tdw(by5_(3s1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -85,6 +86,9 @@ DATABASES = {
         'PASSWORD': 'qwerty'
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
